fe4de8285ee0833f357652d3a1d1cd58e297b4aa
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2022-03-03 14:34:15 +0600

    StudentBatis

---
+++ src/main/java/com/example/Eschool/Mapper/StudentMyBatisRepository.java
@@ -0,0 +1,24 @@
+package com.example.Eschool.Mapper;
+
+import com.example.Eschool.Entity.Student;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface StudentMyBatisRepository {
+    @Insert("INSERT INTO students values (null,#{age},#{firstname},#{phone},#{surname});")
+    int insertStudent(Student student);
+
+    @Select("SELECT * FROM students WHERE id = #{id}")
+    Student findById(Long id);
+
+    @Select("select * from students")
+    List<Student> findAll();
+
+    @Update("update students set age=#{age}, firstname=#{firstname}, surname=#{surname} where id=#{id};")
+    int updateStudent(Student student);
+
+    @Delete("delete from students where id=#{id}")
+    int deleteById(Long id);
+}
