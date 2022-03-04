9364c78bc0048124cb1d10716f5465975a6dbfe4
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2022-03-04 10:01:03 +0600

    readme

--- src/test/java/com/example/Eschool/TestNG.java
+++ src/test/java/com/example/Eschool/TestNG.java
@@ -2,11 +2,15 @@ package com.example.Eschool;
 
 import com.example.Eschool.Service.StudentService;
 import com.example.Eschool.Service.StudentServiceImpl;
+import com.example.Eschool.Service.UserService;
+import com.example.Eschool.Service.UserServiceImpl;
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,9 +26,6 @@ public class TestNG extends AbstractTestNGSpringContextTests {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @Autowired
-    private StudentService studentService;
-
     private MockMvc mockMvc;
 
     @BeforeClass
@@ -36,7 +37,20 @@ public class TestNG extends AbstractTestNGSpringContextTests {
     @BeforeMethod
     public void checkServer() throws Exception {
         System.out.println("checkServer is OK");
-//        this.mockMvc.perform(get("/getStudents")).andDo(print()).andExpect(status().isOk());
-
     }
+
+    @Test
+    public void checkStudentService(){
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(ESchoolApplication.class);
+        StudentService studentService=context.getBean(StudentService.class);
+        Assert.assertNotNull(studentService.getStudentById(1L));
+    }
+
+    @Test
+    public void userService(){
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(ESchoolApplication.class);
+        UserService userService=context.getBean(UserService.class);
+        Assert.assertNotNull(userService.getUserByUsername("user"));
+    }
+
 }
